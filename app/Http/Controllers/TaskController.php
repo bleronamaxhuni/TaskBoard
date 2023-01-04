6572962517742bad2495f0addff6853bf2609a08
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2023-01-04 18:44:35 +0100

    Created structure of model and  controller for tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
+use Carbon\Carbon;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
