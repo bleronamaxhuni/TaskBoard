46a23c514fdee3e3e12bffd93df2eb2fd2229e0a
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2023-01-06 13:05:41 +0100

    tailwind configuration

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name')}}</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     @if (Session::has('message'))
@@ -25,7 +26,11 @@
         <br>
         <textarea type="text" placeholder="Task Description" name="task_description" required></textarea>
         <br>
-        <button type="submit">Create</button>
+        <br>
+        <input  type="date" name="published_at" value="{{old('published_at')}}" required class="border-solid border-2 border-black">
+        <br>
+        <br>
+        <button type="submit" class="border-solid border-2 border-black">Create</button>
     </form>
 
     @foreach ($tasks as $task )
@@ -34,6 +39,15 @@
             Title: {{ $task->task_title }}
         <br>
             Description: {{ $task->task_description }}
+        <br>
+            Published At: {{ $task->published_at }}
+        <br>
+        <button class="border-solid border-2 border-black"><a href="/tasks/create/{{ $task['id'] }}">Edit</a></button>
+        <form action="/tasks/create/{{ $task['id'] }}" method="POST" >
+            @csrf
+            @method('DELETE')
+            <input type="submit" name="" value="delete" class="border-solid border-2 border-black">
+        </form>
         <br>
         <br>
         @endforeach
