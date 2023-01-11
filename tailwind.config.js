50e5fc01802cbdb824039671386401607918947a
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2023-01-11 21:04:10 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
   ],
   theme: {
+    screens: {
+
+			'2xl': {'max': '1980px'},
+			'xl': {'max': '1536px'},	  
+			'lg': {'max': '1023px'},
+			'md': {'max': '767px'},
+			'sm': {'max': '639px'},
+		  },
     extend: {},
   },
   plugins: [],
