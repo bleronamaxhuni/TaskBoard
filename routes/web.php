c4df003cb2957467d76ecbc5448af94eab7b1f37
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2023-01-25 12:48:13 +0100

    added routes to middleware

--- routes/web.php
+++ routes/web.php
@@ -18,30 +18,26 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('dashboard.home');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Tasks
+    Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::post('tasks/create', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit');
+    Route::patch('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::patch('tasks/{task}/completed', [TaskController::class, 'completed'])->name('tasks.completed');
+    // Tags
+    Route::get('tags', [TagsController::class, 'index'])->name('tags.index');
+    Route::post('tags/create', [TagsController::class, 'store'])->name('tags.store');
+    Route::get('tags/{tag}/edit', [TagsController::class, 'edit'])->name('tags.edit');
+    Route::patch('tags/{tag}', [TagsController::class, 'update'])->name('tags.update');
+    Route::delete('tags/{tag}', [TagsController::class, 'destroy'])->name('tags.destroy');
 });
-// Tasks
-Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::post('tasks/create', [TaskController::class, 'store'])->name('tasks.store');
-Route::get('tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit');
-Route::patch('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-Route::patch('tasks/{task}/completed', [TaskController::class, 'completed'])->name('tasks.completed');
-
-// Tags
-Route::get('tags', [TagsController::class, 'index'])->name('tags.index');
-Route::post('tags/create', [TagsController::class, 'store'])->name('tags.store');
-Route::get('tags/{tag}/edit', [TagsController::class, 'edit'])->name('tags.edit');
-Route::patch('tags/{tag}', [TagsController::class, 'update'])->name('tags.update');
-Route::delete('tags/{tag}', [TagsController::class, 'destroy'])->name('tags.destroy');
 
 require __DIR__.'/auth.php';
