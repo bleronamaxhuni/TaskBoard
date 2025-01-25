c475a421911bed54f5483a3e99f69b7e6d36c864
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2025-01-25 22:58:52 +0100

    Added notification feature

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import Alpine from 'alpinejs'
+
+// Wait for Echo to be initialized
+document.addEventListener('DOMContentLoaded', () => {
+    window.Echo = window.Echo;
+    console.log('Echo initialized:', window.Echo);
+});
+
+window.Alpine = Alpine
+Alpine.start()
